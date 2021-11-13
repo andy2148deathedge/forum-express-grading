a40d6592639511855e6179f1d32c93a3b313bd59
andy2148deathedge <andy2148deathedge@gmail.com>
2021-11-13 16:43:37 +0800

    feat: add admin update & delete feature with method-override

--- routes/index.js
+++ routes/index.js
@@ -31,17 +31,21 @@ module.exports = (app, passport) => {
     return res.redirect('/signin')
   }
 
+  // user
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
+  // administrator
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
-  
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
-
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+  app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
+  // signup & signin
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 
