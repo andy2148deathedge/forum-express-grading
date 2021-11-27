e571235dcc543af50e727fc3c1b8f06ede657bd4
andy2148deathedge <andy2148deathedge@gmail.com>
2021-11-27 14:29:12 +0800

    refacor: rebulid admin putRestaurant in api

--- services/adminService.js
+++ services/adminService.js
@@ -51,6 +51,44 @@ const adminService = {
     .then(restaurant => cb( { restaurant: restaurant.toJSON() } ))
   },
 
+  putRestaurant: (req, res, cb) => {
+    if (!req.body.name)
+    return cb({ status: 'error', message: "name didn't exist" })
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.findByPk(req.params.id)
+          .then((restaurant) => {
+            restaurant.update({
+              name: req.body.name,
+              tel: req.body.tel,
+              address: req.body.address,
+              opening_hours: req.body.opening_hours,
+              description: req.body.description,
+              image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
+            })
+            .then(restaurant => cb({ status: 'success', message: 'restaurant was successfully created' }))
+          })
+      })
+    } else {
+      return Restaurant.findByPk(req.params.id)
+        .then((restaurant) => {
+          restaurant.update({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: restaurant.image,
+            CategoryId: req.body.categoryId
+          }).then(restaurant => cb({ status: 'success', message: 'restaurant was successfully created' }))
+        })
+    }
+  },
+
   deleteRestaurant: (req, res, cb) => {
     return Restaurant.findByPk(req.params.id)
       .then(restaurant => 
